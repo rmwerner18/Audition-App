92326ad5e028d51b8a1641b39ba20a0d8ecd5000
Jake Mills <jake.mills.05@gmail.com>
2020-08-17 11:48:52 -0400

    Updated actor controller with index/show/new/create/edit/update/actor_params/find_actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,26 +1,29 @@
 class ActorsController < ApplicationController
     def index
-
+        @actors = Actor.all
     end
 
     def show
-
+        find_actor
     end
 
     def new
-
+        @actor = Actor.new
     end
 
     def create
-
+        @actor = Actor.create(actor_params)
+        redirect_to @actor
     end
 
     def edit
-
+        find_actor
     end
 
     def update
-
+        find_actor
+        @actor.update(actor_params)
+        redirect_to @actor
     end
 
     def delete
@@ -29,5 +32,12 @@ class ActorsController < ApplicationController
 
     private
 
+    def actor_params
+        params.require(:actor).permit(:name, :sex, :age)
+    end
+
+    def find_actor
+        @actor = Actor.find(params[:id])
+    end
     
 end
