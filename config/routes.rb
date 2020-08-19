97e0997b4803fe3ceb84dcc3d9661ac31459e594
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-19 09:38:40 -0500

    make trial authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   patch 'requests/:id', to: 'requests#update', as: 'update_request'
   resources :auditions, except: [:create]
   post 'auditions', to: 'auditions#create', as: 'create_audition'
-  resources :actors
-  resources :events
-  resources :casting_agents
   devise_for :actors, path: 'actors', controllers: {
     sessions: 'actors/sessions'
   }
   devise_for :casting_agents, path: 'casting_agents', controllers: {
     sessions: 'casting_agents/sessions'
   }
+  resources :actors
+  resources :events
+  resources :casting_agents
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
