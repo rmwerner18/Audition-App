de79adf7e633f3a8b7f31cad1abde7279e78c2f5
Jake Mills <jake.mills.05@gmail.com>
2020-08-20 11:51:39 -0400

    Started creating Seed Data

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.1'
 
 gem 'devise'
-gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+gem 'faker', :git => 'https://https://github.com/JMHero05/faker', :branch => 'master'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,22 @@
 # CastingAgent.delete_all
 # Event.delete_all
 
-# 10.times do 
-#     CastingAgent.create(name: Faker::Name.name)
-# end
+CastingAgent.delete_all
+Actor.delete_all
+Event.delete_all
 
 10.times do 
-    Actor.create(name: Faker::Name.name, sex: Faker::Gender.binary_type, age: Faker::Number.between(from: 18, to: 100))
+    CastingAgent.create!(name: Faker::Name.name, agency: Faker::Company.name, email: Faker::Internet.unique.email(name: Faker::Name.name, domain: 'example'), username: Faker::Internet.unique.username, password: 'audition', password_confirmation: 'audition')
 end
 
-# Event.delete_all
+30.times do 
+    Actor.create!(name: Faker::Name.name, sex: Faker::Gender.binary_type, age: Faker::Number.between(from: 18, to: 100), email: Faker::Internet.unique.email(name: Faker::Name.name, domain: 'example'), username: Faker::Internet.unique.username, password: 'audition', password_confirmation: 'audition')
+end
+
+10.times do
+    Event.create!(title: Faker::Theatre::Musicals.title, )
+end
 
 # 10.times do 
 #     Event.create(title: Faker::Music::Opera.verdi, date: Faker::Date.forward(days: 23), casting_agent_id: 2)
-# end
-
-
-# 10.times do 
-#     Event.create(title: Faker::Music::Opera.verdi, date: Faker::Date.forward(days: 23), casting_agent_id: 3)
 # end
